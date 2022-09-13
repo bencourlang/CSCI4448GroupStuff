f25a2d1866b25027c8b6e3ea4235b443194699bf
Ben Courlang <bencourlang@cu-tcom-1-10.203.3.126.int.colorado.edu>
2022-09-13 15:17:45 -0600

    gsafs

--- 2.2/RotLA.java
+++ 2.2/RotLA.java
@@ -12,6 +12,8 @@ public class RotLA{
         int y_axis_length = 3;
         int z_axis_length = 3;	
         ArrayList<ArrayList<ArrayList<String>>> map = new ArrayList<>(x_axis_length);
+        
+        //Change string to arraylist to store multiple things in each section of map
 
         for (int i = 0; i < x_axis_length; i++) {
             map.add(new ArrayList<ArrayList<String>>(y_axis_length));
@@ -51,7 +53,7 @@ public class RotLA{
             System.out.println();
         }
 
-        System.out.println("n, s, e, w, u, d");
+        //System.out.println("n, s, e, w, u, d");
         String dir = s.nextLine();
 
         while(dir != null){
@@ -72,9 +74,11 @@ public class RotLA{
                     }
                 }
             }
+
+            Schmoovement thing = new Schmoovement(pPos);
     
             if(pPos.get(1) == 1 && pPos.get(2) == 1){
-                System.out.println("n, s, e, w, u, d");
+                //System.out.println("n, s, e, w, u, d");
                 if(dir.equals("n")){
                     System.out.println("north");
         
@@ -146,7 +150,7 @@ public class RotLA{
                 }
             }
             else{
-                System.out.println("n, s, e, w");
+                //System.out.println("n, s, e, w");
                 if(dir.equals("n")){
                     System.out.println("north");
         
@@ -205,4 +209,124 @@ public class RotLA{
             dir = s.nextLine();
         }
     }
+}
+
+class RandMove{
+    /*RandMove(ArrayList<Integer> opt){
+
+    }*/
+
+    public static int randDir(ArrayList<Integer> opt){
+        int movDir = -1;
+        Random r = new Random();
+        return opt.get(r.nextInt(opt.size()-1));
+    }
+
+    /*public static void oop(){
+        Random r = new Random();
+        for(int i = 0; i < 10; i++){
+            System.out.print(r.nextInt(10) + " ");
+        }
+        System.out.println();
+    }*/
+}
+
+class Schmoovement{
+    int x;
+    int y;
+    int z;
+    //static ArrayList<String> potDir = new ArrayList<>();
+    static ArrayList<Integer> potDir = new ArrayList<>();
+
+    Schmoovement(ArrayList<Integer> xyz){
+        potDir.clear();
+
+        x = xyz.get(0);
+        y = xyz.get(1);
+        z = xyz.get(2);
+
+        canNorth(y);
+        canSouth(y);
+        canEast(z);
+        canWest(z);
+        canUp(x);
+        canDown(x);
+
+        System.out.println(potDir);
+    }
+
+    public static void move(int dir){
+        switch(dir){
+            case 1: //move north save status ? update status
+                /*map.get(x).get(y).set(z, store);
+                store = map.get(x).get(y-1).get(z);
+                map.get(x).get(y-1).set(z, p);*/
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    public static void canNorth(int y){
+        if(y != 0){
+            potDir.add(1);
+        }
+    }
+    public static void canSouth(int y){
+        if(y != 2){
+            potDir.add(2);
+        }
+    }
+    public static void canEast(int z){
+        if(z != 2){
+            potDir.add(3);
+        }
+    }
+    public static void canWest(int z){
+        if(z != 0){
+            potDir.add(4);
+        }
+    }
+    public static void canUp(int x){
+        if(x != 3){
+            potDir.add(5);
+        }
+    }
+    public static void canDown(int x){
+        if(x != 0){
+            potDir.add(6);
+        }
+    }
+
+    /*public static void canNorth(int y){
+        if(y != 0){
+            potDir.add("North");
+        }
+    }
+    public static void canSouth(int y){
+        if(y != 2){
+            potDir.add("South");
+        }
+    }
+    public static void canEast(int z){
+        if(z != 2){
+            potDir.add("East");
+        }
+    }
+    public static void canWest(int z){
+        if(z != 0){
+            potDir.add("West");
+        }
+    }
+    public static void canUp(int x){
+        if(x != 3){
+            potDir.add("Up");
+        }
+    }
+    public static void canDown(int x){
+        if(x != 0){
+            potDir.add("Down");
+        }
+    }*/
 }
